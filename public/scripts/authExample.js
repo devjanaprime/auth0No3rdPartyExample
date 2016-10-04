f34fa6209e25ada2f829db07043c7bcf6ae4f481
devjanaprime <dev@primeacademy.io>
2016-10-04 11:57:09 -0500

    basics working. needs readme update

---
+++ public/scripts/authExample.js
@@ -0,0 +1,69 @@
+// var lock = new Auth0Lock( 'CLIENTID', 'DOMAIN');
+// log out url, from Auth0
+// var logOutUrl = 'https://YOURAUTH0SUBDOMAIN.auth0.com/v2/logout';
+var lock = new Auth0Lock( 'k7cxIuaUm4Y59SDhjWD2SOJNra7yTjsw', 'devjanaprime.auth0.com' );
+var logOutUrl = 'https://devjanaprime.auth0.com/v2/logout';
+
+var myApp=angular.module( 'myApp', [] );
+
+// basic controller
+myApp.controller( 'authController', [ '$scope', '$http', function( $scope, $http ){
+
+  //run at controller load
+  $scope.init = function(){
+    console.log( 'in init' );
+    // check if a user's info is saved in localStorage
+    if( JSON.parse( localStorage.getItem( 'userProfile' ) ) ){
+      // if so, save userProfile as $scope.userProfile
+      $scope.userProfile = JSON.parse( localStorage.getItem( 'userProfile' ) );
+      console.log( 'loggedIn:', $scope.userProfile );
+      $scope.showUser = true;
+    }
+    else{
+      // if not, make sure we are logged out and empty
+      emptyLocalStorage();
+      $scope.showUser = false;
+    }
+  } // end init function
+
+  $scope.logIn = function(){
+    // call out logIn function from auth0.js
+    console.log( 'in logIn' );
+    lock.show( function( err, profile, token ) {
+      if (err) {
+        console.error( "auth error: ", err);
+      } // end error
+      else {
+        // save token to localStorage
+        localStorage.setItem( 'userToken', token );
+        // save user profile to localStorage
+        localStorage.setItem( 'userProfile', JSON.stringify( profile ) );
+        // reload page because dirtyhaxorz
+        location.reload();
+      } // end no error
+    }); //end lock.show
+  }; // end scope.logIn
+
+  $scope.logOut = function(){
+    // call our logOutUrl
+    $http({
+      method:'GET',
+      url: logOutUrl,
+    }).then( function( data ){
+      // if logged out OK
+      if( data.data == 'OK' ){
+        // empty localStorage
+        emptyLocalStorage();
+        $scope.showUser = false;
+      }
+    })
+  }; // end scope.logIn
+
+  // run init on controller load
+  $scope.init();
+}]); // end authController
+
+var emptyLocalStorage = function(){
+  localStorage.removeItem( 'userProfile' );
+  localStorage.removeItem( 'userToken' );
+}; // end emptyLocalStorage
---
+++ readme.md
@@ -0,0 +1,6 @@
+Auth0 no 3rd Party Example
+==========================
+
+Example of setting up Auth0 authorization that doesn't use any 3rd party for authorization.
+
+Built on top of: https://github.com/devjanaprime/auth0Example
